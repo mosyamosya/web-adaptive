1398f2feee5bc4ab51651606dd7333b7f9927317
mosyamosya <93034139+mosyamosya@users.noreply.github.com>
2024-05-23 15:44:59 +0200

    4

---
+++ adaptweb4/adaptweb4/Book.cs
@@ -0,0 +1,48 @@
+﻿namespace adaptweb4
+{
+    internal class Book
+    {
+        public int id;
+        private string name;
+        protected bool isread;
+        protected internal double rate;
+        internal int npages;
+        public Book(int id, string name, int npages, double rate, bool isread)
+        {
+            this.id = id;
+            this.name = name;
+            this.npages = npages;
+            this.rate = rate;
+            this.isread = isread;
+           
+        }
+        public void WriteTextRate()
+        {
+            switch (rate)
+            {
+                case double r when r > 8 && r <= 10:
+                    Console.WriteLine("Найкраща книга у світі");
+                    break;
+                case double r when r > 6 && r <= 8:
+                    Console.WriteLine("Непогана книга");
+                    break;
+                case double r when r > 4 && r <= 6:
+                    Console.WriteLine("не дуже цікава книга");
+                    break;
+                case double r when r <= 4:
+                    Console.WriteLine("Погана книга");
+                    break;
+            }
+        }
+
+        internal int GetNpages()
+        {
+            return npages;
+        } 
+        public void WriteInfo(Book book)
+        {
+            string isreadMess = isread ? "Прочитана" : "Не прочитана";
+            Console.WriteLine($"{name} має {npages} сторінок, рейтинг - {rate}, статус книги для вас - {isreadMess}"); 
+        }
+    }
+}
---
+++ adaptweb4/adaptweb4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using adaptweb4;
+using Microsoft.VisualBasic.FileIO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book Harry_Potter_book = new Book(1, "Harry Potter", 399, 9.8, true);
+        Book Day_Of_Dragon_book = new Book(2, "Day of Dragon", 448, 7.8, false);
+
+        Type booktype = typeof (Book);
+        TypeInfo booktypeinfo = booktype.GetTypeInfo();
+
+        Console.WriteLine($"Назва типу: {booktype.Name}");
+        Console.WriteLine("\n учасники:");
+        foreach (MemberInfo memb in booktypeinfo.GetMembers())
+        {
+            Console.WriteLine($"Ім'я: {memb.Name}({memb.ReflectedType})");
+        }
+
+        Console.WriteLine("\nПоля:");
+        foreach (FieldInfo fieldInfo in booktypeinfo.DeclaredFields)
+        {
+            Console.WriteLine($"Ім'я поля: {fieldInfo.Name} ({fieldInfo.Attributes}), FieldType: {fieldInfo.FieldType}");
+        }
+        Console.WriteLine("\nMетоди:");
+        foreach (MethodInfo methodInfo in booktypeinfo.DeclaredMethods)
+        {
+            Console.WriteLine($"Ім'я метода: {methodInfo.Name}({methodInfo.Attributes}), MethodType: {methodInfo.ReturnType}");
+        }
+
+        Console.WriteLine("\nReflection:");
+        MethodInfo reflectionMethod = booktype.GetMethod("WriteInfo");
+        if (reflectionMethod != null)
+        {
+            reflectionMethod.Invoke(Harry_Potter_book, new object[] { Day_Of_Dragon_book });
+        }
+    }
+}
